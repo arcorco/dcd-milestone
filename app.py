c162b24af4de98ee22488a039872ac10d7917764
Ubuntu <ubuntu@ip-172-31-86-198.ec2.internal>
2020-03-15 15:49:14 +0000

    templates folder added with templates base.html and games.html. Carousel added to games.html.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DNAME"] = 'game_review'
+app.config["MONGO_URI"] = 'mongodb+srv://first_user:WG9zKqgDwfDTHNa@myfirstcluster-74ub4.mongodb.net/game_review?retryWrites=true&w=majority'
+#environment variable for username and password
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/get_games')
+def get_tasks():
+    return render_template("games.html", games=mongo.db.games.find())
     
 
 if __name__ == "__main__":
