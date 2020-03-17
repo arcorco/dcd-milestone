6d8b8dd24a5a2087740b22f30da9b7e13e5544d0
Ubuntu <ubuntu@ip-172-31-86-198.ec2.internal>
2020-03-17 16:35:26 +0000

    Add game form saves new game to MongoDB database

--- app.py
+++ app.py
@@ -13,12 +13,29 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_games')
-def get_tasks():
-    return render_template("games.html", games=mongo.db.games.find())
+def get_games():
+    return render_template('games.html', games=mongo.db.games.find())
     
 @app.route('/add_game')
 def add_game():
-    return render_template("addgame.html")
+    return render_template('addgame.html')
+
+@app.route('/insert_game', methods=['POST'])
+def insert_game():
+    games = mongo.db.games
+    new_game = request.form.to_dict()
+    min_players = new_game.get("number_of_players_min")
+    max_players = new_game.get("number_of_players_max")
+    min_age = new_game.get("age_range")
+    age = "%s+" % min_age
+    players = "%s-%s" % (min_players, max_players)
+    new_game.update( {'number_of_players' : players} )
+    new_game.update( {'age_range' : age} )
+    new_game.pop('number_of_players_min')
+    new_game.pop('number_of_players_max')
+    
+    games.insert_one(new_game)
+    return redirect(url_for('get_games'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
