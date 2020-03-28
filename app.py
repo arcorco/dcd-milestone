3d93004a220b5716756f4a823b5817dcf6a518af
Ubuntu <ubuntu@ip-172-31-86-198.ec2.internal>
2020-03-28 17:13:50 +0000

    Navbar added with mobile view

--- app.py
+++ app.py
@@ -107,6 +107,10 @@ def update_review(review_id):
         })
     return redirect(url_for('get_reviews'))
 
+@app.route('/delete_review/<review_id>')
+def delete_review(review_id):
+    mongo.db.reviews.remove({'_id': ObjectId(review_id)})
+    return redirect(url_for('get_reviews'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
