32420f0313a288e746c87711d43352e536ed060b
Ubuntu <ubuntu@ip-172-31-86-198.ec2.internal>
2020-03-30 15:07:44 +0000

    Validation added to forms with javascript

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ def delete_review(review_id):
 @app.route('/game_review/<game_name>')
 def game_review(game_name):
     return render_template('gamereviews.html', reviews=mongo.db.reviews.find({"game_name": game_name}))
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
