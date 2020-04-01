1fc060f5dbac87e3d5e2be500912217731eee4a5
Ubuntu <ubuntu@ip-172-31-86-198.ec2.internal>
2020-04-01 12:17:28 +0000

    app.py updated to fix deployment error

--- app.py
+++ app.py
@@ -166,5 +166,4 @@ def game_review(game_name):
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+            port=int(os.environ.get('PORT')))
